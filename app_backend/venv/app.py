91895cec454eaf51d2e8c884199a59bd99cfd1a0
Ivan Anic <i.anic779@gmail.com>
2022-02-06 14:58:27 +0100

    read base64 from get request

--- app_backend/venv/app.py
+++ app_backend/venv/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import base64
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/get", methods=['GET'])
 def index():
-    return "Hello World!"
+
+    # Read base64 from url if it exists, save it to base64_query
+    base64_query = request.args.get('query')
+
+    # If base64_query is not empty, decode it and save it to base64_decoded
+    if base64_query:
+        base64_decoded = base64.b64decode(base64_query)
+
+        return base64_decoded
+
+    # If base64_query is empty, return error
+    else:
+        return "Error: No base64 query found"
