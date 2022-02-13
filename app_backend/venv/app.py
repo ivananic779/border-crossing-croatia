db66059a133a5f3eaf169fc91e7d74179e2ee77d
Ivan Anic <i.anic779@gmail.com>
2022-02-13 02:44:34 +0100

    Backend - optional query type

--- app_backend/venv/app.py
+++ app_backend/venv/app.py
@@ -10,19 +10,31 @@ app = Flask(__name__)
 CORS(app)
 
 table_names = {
-    'Ukupno ljudi': 't_ukupno',
-    'Autobusi': 't_autobusi',
-    'Osobni automobili': 't_osobni_automobili',
-    'Avioni': 't_avioni',
-    'Teretna vozila': 't_teretna_vozila',
-    'Plovila': 't_plovila',
-    'Vlakovi': 't_vlakovi',
-    'Ukupno prijevozna sredstva': 't_ukupno_prijevozna_sredstva',
-    'Cestovni': 't_cestovni',
-    'Pomorski': 't_pomorski',
-    'Rijecni': 't_rijecni',
-    'Zeljeznicki': 't_zeljeznicki',
-    'Zracni': 't_zracni'
+    "Ukupno ljudi": "t_ukupno",
+    "Autobusi": "t_autobusi",
+    "Osobni automobili": "t_osobni_automobili",
+    "Avioni": "t_avioni",
+    "Teretna vozila": "t_teretna_vozila",
+    "Plovila": "t_plovila",
+    "Vlakovi": "t_vlakovi",
+    "Ukupno prijevozna sredstva": "t_ukupno_prijevozna_sredstva",
+    "Cestovni": "t_cestovni",
+    "Pomorski": "t_pomorski",
+    "Rijecni": "t_rijecni",
+    "Zeljeznicki": "t_zeljeznicki",
+    "Zracni": "t_zracni",
+}
+
+query_types = {
+    "Neto ukupno": ["ulaz_ukupno", ", izlaz_ukupno as second_value"],
+    "Neto domaci": ["ulaz_domaci", ", izlaz_domaci as second_value"],
+    "Neto strani": ["ulaz_strani", ", izlaz_strani as second_value"],
+    "Ulaz ukupno": ["ulaz_ukupno", ", ulaz_ukupno as second_value"],
+    "Izlaz ukupno": ["izlaz_ukupno", ", izlaz_ukupno as second_value"],
+    "Ulaz domaci": ["ulaz_domaci", ", ulaz_domaci as second_value"],
+    "Izlaz domaci": ["izlaz_domaci", ", izlaz_domaci as second_value"],
+    "Ulaz strani": ["ulaz_strani", ", ulaz_strani as second_value"],
+    "Izlaz strani": ["izlaz_strani", ", izlaz_strani as second_value"],
 }
 
 # This is used to keep all values for a single graph line
@@ -31,13 +43,44 @@ table_names = {
 # Different functions utilize this differentrly
 current_line_values = []
 
-def get_net_value_change(_ulaz, _izlaz):
+
+def get_net_value_change(_first_value, _second_value):
     global current_line_values
 
     # Add new value to current_line_values
-    current_line_values.append(_ulaz - _izlaz)
+    current_line_values.append(_first_value - _second_value)
 
     return sum(current_line_values)
+
+
+def get_query_value_types(_query_type):
+    global query_types
+
+    return query_types[_query_type]
+
+
+def get_graph_value(_first_value, _second_value, _query_type):
+    global current_line_values
+
+    if _query_type == "Neto ukupno":
+        return get_net_value_change(_first_value, _second_value)
+    elif _query_type == "Neto domaci":
+        return get_net_value_change(_first_value, _second_value)
+    elif _query_type == "Neto strani":
+        return get_net_value_change(_first_value, _second_value)
+    elif _query_type == "Ulaz ukupno":
+        return _first_value
+    elif _query_type == "Izlaz ukupno":
+        return _second_value
+    elif _query_type == "Ulaz domaci":
+        return _first_value
+    elif _query_type == "Izlaz domaci":
+        return _second_value
+    elif _query_type == "Ulaz strani":
+        return _first_value
+    elif _query_type == "Izlaz strani":
+        return _second_value
+
 
 def get_graph(_query_options):
     global current_line_values
@@ -52,24 +95,42 @@ def get_graph(_query_options):
     for _query_option in _query_options:
         current_line_values = []
 
-        data = {
-            "name": _query_option["name"],
-            "series": []
-        }
+        data = {"name": _query_option["name"], "series": []}
+
+        if _query_option["query_type"] != "":
+            first_query_value, second_query_value = get_query_value_types(
+                _query_option["query_type"]
+            )
 
         try:
-            conn.execute("SELECT date, ulaz_ukupno, izlaz_ukupno FROM " + get_table_name(_query_option["table"]) + " where date between '" + _query_option["date_from"] + "' and '" + _query_option["date_to"] + "' order by date")
+            conn.execute(
+                "SELECT date, "
+                + first_query_value
+                + " as first_value"
+                + second_query_value
+                + " FROM "
+                + get_table_name(_query_option["table"])
+                + " where date between '"
+                + _query_option["date_from"]
+                + "' and '"
+                + _query_option["date_to"]
+                + "' order by date"
+            )
 
             # Check if response is empty
             if conn.rowcount == 0:
                 return query_error()
 
-            for date, ulaz_ukupno, izlaz_ukupno in conn.fetchall():          
+            for date, first_value, second_value in conn.fetchall():
 
-                data["series"].append({
-                    "name": date.strftime("%d-%m"),
-                    "value": get_net_value_change(ulaz_ukupno, izlaz_ukupno),
-                })
+                data["series"].append(
+                    {
+                        "name": date.strftime("%d-%m"),
+                        "value": get_graph_value(
+                            first_value, second_value, _query_option["query_type"]
+                        ),
+                    }
+                )
 
             ret.append(data)
 
@@ -79,12 +140,14 @@ def get_graph(_query_options):
 
     return jsonify_formatted_db_response(ret)
 
+
 def get_table_name(_key):
     global table_names
 
     return table_names[_key]
 
-@app.route("/api/get", methods=['POST'])
+
+@app.route("/api/get", methods=["POST"])
 def index():
 
     # Read data from post body
@@ -95,6 +158,7 @@ def index():
         return post_body_error()
 
     return get_graph(query_options)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app_backend/venv/errors.py
+++ app_backend/venv/errors.py
@@ -1,10 +1,13 @@
 from flask.json import jsonify
 
+
 def query_error():
-    return jsonify({'error': 'QUERY_ERROR'})
+    return jsonify({"error": "QUERY_ERROR"})
+
 
 def post_body_error():
-    return jsonify({'error': 'POST_BODY_ERROR'})
+    return jsonify({"error": "POST_BODY_ERROR"})
+
 
 def connect_db_error():
-    return jsonify({'error': 'CONNECT_DB_ERROR'})
+    return jsonify({"error": "CONNECT_DB_ERROR"})
